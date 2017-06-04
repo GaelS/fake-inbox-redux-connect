1c5695c612c0cf088838add499e785880417ff04
GaelS <servauga@gmail.com>
2017-06-04 14:15:53 +0200

    remove log

--- src/react-redux/connect.js
+++ src/react-redux/connect.js
@@ -9,7 +9,6 @@ export default function(mapDispatchToProps, mapStateToProps) {
         this.context.store.subscribe( () => this.forceUpdate());
       }
       render() {
-        console.log(this)
         const {context, props} = this;
         const propsFromDispatch = mapDispatchToProps(context.store.dispatch);
         const propsFromState = mapStateToProps(context.store.getState());
